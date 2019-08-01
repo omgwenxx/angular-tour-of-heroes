0b25b34ecc845702df7ad1a457cb78cda05228c6
Gwendolyn Rippberger <grippberger@grippberger.local>
2019-08-01 07:58:53 +0200

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Black Widow' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Daredevil' },
+  { id: 17, name: 'Jessica Jones' },
+  { id: 18, name: 'Ironfist' },
+  { id: 19, name: 'Punisher' },
+  { id: 20, name: 'Deadpool' }
+];
